5b8c4972bee51ea80c5830ff75edb32c1ac19626
Milla-Duke <ella.wilks@nzme.co.nz>
2026-07-13 20:33:52 +1200

    Update slack.js

--- api/slack.js
+++ api/slack.js
@@ -25,7 +25,7 @@ const crypto = require('crypto');
 
 const GITHUB_REPO         = "Milla-Duke/visuals-scheduler";
 const TEAMUP_VISUALS_ID   = 11087400;
-const TEAMUP_CALENDAR_KEY = "ksi7k2xr9brt5tn2ac";
+const TEAMUP_CALENDAR_KEY = "q1rqrs";
 const NOTIFY_CHANNEL      = "visuals-team-chat-24";
 
 // Maps TeamUp 'who' field names to Slack User IDs for @mentions and DMs
