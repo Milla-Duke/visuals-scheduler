a41315b130a18099e4caec4721ffd62b0e3c4aa5
Milla-Duke <ella.wilks@nzme.co.nz>
2026-07-13 20:32:46 +1200

    Update visuals_monday_draft.py

--- visuals_monday_draft.py
+++ visuals_monday_draft.py
@@ -45,7 +45,7 @@ except (FileNotFoundError, json.JSONDecodeError):
 SLACK_BOT_TOKEN   = _config.get("slack_bot_token") or os.environ.get("SLACK_BOT_TOKEN", "")
 TEAMUP_API_KEY    = _config.get("teamup_api_key")  or os.environ.get("TEAMUP_API_KEY", "")
 
-TEAMUP_CALENDAR_KEY       = "ksi7k2xr9brt5tn2ac"
+TEAMUP_CALENDAR_KEY       = "q1rqrs"
 TEAMUP_VISUALS_ID         = 11087400
 TEAMUP_STUDIO_ID          = 11087384
 TEAMUP_EDITING_ID         = 12991604
